83d90e02fec7b6a9cc6e4dd8e79b8742820167ac
guilherme <guilherme.tec1107@gmail.com>
2025-07-07 20:57:03 -0300

    fixing my test

--- src/test/java/com/streaming/backend/VideoControllerTest.java
+++ src/test/java/com/streaming/backend/VideoControllerTest.java
@@ -1,8 +1,10 @@
 package com.streaming.backend;
 
+import com.streaming.backend.config.VideoStorageConfig;
 import com.streaming.backend.models.Video;
 import com.streaming.backend.repositories.VideoRepository;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,6 +12,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -32,6 +36,20 @@ public class VideoControllerTest {
     @Autowired
     private VideoRepository videoRepository;
 
+    @BeforeEach
+    public void clean() throws IOException {
+        Path uploadDir = VideoStorageConfig.UPLOAD_DIR;
+
+        Files.createDirectories(uploadDir);
+
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(uploadDir)) {
+            for (Path file : stream) {
+                Files.deleteIfExists(file);
+            }
+        }
+
+    }
+
     @Test
     public void shouldSaveVideoInTheCorrectLocationWithNameBasedOnId() throws Exception {
         MockMultipartFile file = new MockMultipartFile(
@@ -45,7 +63,7 @@ public class VideoControllerTest {
         assertEquals(1, videos.size());
 
         Video video = videos.get(0);
-        Path expectedPath = Paths.get("/var/www/videos", video.getId() + ".mp4");
+        Path expectedPath = VideoStorageConfig.UPLOAD_DIR.resolve(video.getId() + ".mp4");
         assertEquals(expectedPath.toString(), video.getPathArchive());
 
         assertTrue(Files.exists(expectedPath));
