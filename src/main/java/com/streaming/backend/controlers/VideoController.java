1f424959cb7ee3b1fef7438a8664d1e2991c9eae
guilherme <guilherme.tec1107@gmail.com>
2025-07-16 13:10:05 -0300

    Logging system

--- src/main/java/com/streaming/backend/controlers/VideoController.java
+++ src/main/java/com/streaming/backend/controlers/VideoController.java
@@ -1,11 +1,12 @@
 package com.streaming.backend.controlers;
 
-import com.streaming.backend.repositories.VideoRepository;
+import com.streaming.backend.services.VideoConversionService;
 import com.streaming.backend.services.VideoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +21,8 @@ public class VideoController {
 
     private final VideoService videoService;
 
+    private static final Logger logger = LoggerFactory.getLogger(VideoConversionService.class);
+
     @Autowired
     public VideoController(VideoService videoService) {
         this.videoService = videoService;
@@ -31,6 +34,7 @@ public class VideoController {
             videoService.processVideoUploaded(file);
             return ResponseEntity.status(HttpStatus.CREATED).build();
         } catch (Exception e) {
+            logger.error("Error when saving video", e);
             return ResponseEntity
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Error when saving the video: " + e.getMessage());
--- src/main/java/com/streaming/backend/services/VideoConversionService.java
+++ src/main/java/com/streaming/backend/services/VideoConversionService.java
@@ -3,13 +3,19 @@ package com.streaming.backend.services;
 import com.streaming.backend.utilities.Utilities;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class VideoConversionService {
+
+    private static final Logger logger = LoggerFactory.getLogger(VideoConversionService.class);
 
     public static ByteArrayInputStream convertToMp4(MultipartFile multipartVideo) throws IOException, InterruptedException {
         String fileExtension = Utilities.getFileExtension(Objects.requireNonNull(multipartVideo.getOriginalFilename()));
@@ -22,10 +28,15 @@ public class VideoConversionService {
         Path tempOutput = Files.createTempFile("converted_", ".mp4");
 
         Process process = getConvertProcess(tempInput, tempOutput);
+        logProcessOutput(process);
 
         int exitCode = process.waitFor();
+
         if (exitCode != 0) {
-            throw new RuntimeException("Error in converting video: " + exitCode); // TODO log
+            logger.error("Error in converting video, code: {}", exitCode);
+            throw new RuntimeException("Error in converting video: " + exitCode);
+        } else {
+            logger.info("Conversion completed successfully, code: {}", exitCode );
         }
 
         byte[] byteConvertedVideo = Files.readAllBytes(tempOutput);
@@ -49,5 +60,16 @@ public class VideoConversionService {
         ProcessBuilder processBuilder = new ProcessBuilder(command);
         processBuilder.redirectErrorStream(true);
         return processBuilder.start();
+    }
+
+    private static void logProcessOutput(Process process) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                logger.info("[FFMPEG] {}", line);
+            }
+        } catch (IOException e) {
+            logger.error("Error reading conversion output: ", e);
+        }
     }
 }
